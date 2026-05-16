a16a53e94ce58330e48e1ee24d13e277d963d551
HaseebAhmad24-collab <70149250@student.uol.edu.pk>
2026-05-16 13:00:22 +0500

    chore: reordered sections to flow About -> Education -> Projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,8 @@ export default function Home() {
       <div className="flex flex-col min-h-screen">
         <Hero />
         <About />
-        <ProjectsBento />
         <Education />
+        <ProjectsBento />
         <Footer />
       </div>
     </>
